e85845344134879fc62df06f9f02817f5a72a792
back and white <01backandwhite@gmail.com>
2026-02-12 21:39:36 +0100

    Ajustes para la generacion del build y la configuracion de la BD

--- src/main/java/com/backandwhite/MicAuthserviceApplication.java
+++ src/main/java/com/backandwhite/MicAuthserviceApplication.java
@@ -1,12 +1,18 @@
 package com.backandwhite;
 
 import com.backandwhite.common.configuration.annotation.EnabledCoreFinolApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnabledCoreFinolApplication
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://mic-auth-production.up.railway.app/", description = "Production Server"),
+        @Server(url = "http://localhost:9001", description = "Local Server")
+})
 public class MicAuthserviceApplication {
 
     @Bean
