044a2f8c90a17bf63a2d3f8516dde77ff2c32b4a
back and white <01backandwhite@gmail.com>
2026-02-13 10:25:58 +0100

    Configuracion de ssl

--- src/main/java/com/backandwhite/application/security/CorsRegistryConfiguration.java
+++ src/main/java/com/backandwhite/application/security/CorsRegistryConfiguration.java
@@ -7,32 +7,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class CorsRegistryConfiguration
-        //implements WebMvcConfigurer
-{
+public class CorsRegistryConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins(
-//                        "http://localhost:4200",
-//                        "http://localhost:8080",
-//                        "https://mic-auth-production.up.railway.app")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
 
     @Bean
     public CorsFilter corsFilter() {
@@ -40,8 +24,7 @@ public class CorsRegistryConfiguration
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "https://mic-auth-production.up.railway.app"
+                "http://localhost:4200", "https://webapp-production-68d2.up.railway.app/"
         ));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
