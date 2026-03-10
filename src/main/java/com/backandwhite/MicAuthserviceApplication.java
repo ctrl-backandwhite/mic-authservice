ac697b7193d166997ee967e843e8c6530e12fdf9
back and white <01backandwhite@gmail.com>
2026-03-10 23:34:18 +0100

    Cambiando anotacion del core

--- src/main/java/com/backandwhite/MicAuthserviceApplication.java
+++ src/main/java/com/backandwhite/MicAuthserviceApplication.java
@@ -1,11 +1,11 @@
 package com.backandwhite;
 
-import com.backandwhite.common.configuration.annotation.EnabledCoreFinolApplication;
+import com.backandwhite.common.configuration.annotation.EnableCoreApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 
-@EnabledCoreFinolApplication
+@EnableCoreApplication
 @OpenAPIDefinition(servers = {
         @Server(url = "https://mic-authservice-production.up.railway.app", description = "Production Server."),
         @Server(url = "https://localhost:8443", description = "Local Server.")
--- src/test/java/com/backandwhite/application/security/SecurityConfigWebTest.java
+++ src/test/java/com/backandwhite/application/security/SecurityConfigWebTest.java
@@ -3,6 +3,7 @@ package com.backandwhite.application.security;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         SecurityConfig.class,
         CorsRegistryConfiguration.class
 })
-@org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
+@AutoConfigureMockMvc
 class SecurityConfigWebTest {
 
     @Autowired
